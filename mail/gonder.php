c15de121984cb0247636ee36bb3d7d56f35688ac
ibrahim <ibrahim.tarla@triusbilisim.com>
2020-03-15 12:57:47 +0300

    added all amigra web site elemenets

---
+++ mail/gonder.php
@@ -0,0 +1,30 @@
+﻿
+<meta http-equiv="refresh" content="2;URL= http://amigrastudio.com">
+
+<?
+require("class.phpmailer.php");
+
+$mail = new PHPMailer();
+
+$mail->IsSMTP();                                   // send via SMTP
+$mail->Host     = "mail.amigrastudio.com"; // SMTP servers
+$mail->SMTPAuth = true;     // turn on SMTP authentication
+$mail->Username = "info@amigrastudio.com";  // SMTP username
+$mail->Password = "AmigraStudio34."; // SMTP password
+$mail->CharSet = 'UTF-8';
+$mail->Port = 587;
+$mail->From     = "info@amigrastudio.com"; // smtp kullanycy adynyz ile ayny olmaly
+$mail->Fromname = "giden ismi";
+$mail->AddAddress("info@amigrastudio.com","Websitenizden gelen Sipariþiniz.");
+$mail->Subject  =  $_POST['isim'];
+$mail->Body     =  implode("  ",$_POST);
+if(!$mail->Send())
+{
+   echo "Mesaj Gönderilemedi <p>";
+   echo "Mailer Error: " . $mail->ErrorInfo;
+   exit;
+}
+
+echo '<script type="text/javascript">alert("E Postanız  Başarı ile  Gönderilmiştir.");</script>';
+
+?>
